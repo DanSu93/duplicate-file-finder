f77933227676d9993a0e7172bda7b0717c5104b6
nairo <nairobi93@mail.ru>
2021-03-28 17:16:58 +0300

    Task in progress

--- cmd/duplicate-file-finder/find_dupliate.go
+++ cmd/duplicate-file-finder/find_dupliate.go
@@ -1,21 +1,86 @@
-package duplicate_file_finder
+// Package provides duplicate file search and duplicate file removal operation
+// Support operations: CheckDuplicate, deleteDuplicate
+
+package duplicateFileFinder
 
 import (
+	"crypto/sha512"
 	"fmt"
 	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sync"
 )
 
-func DeleteDuplicateFiles(dir string) []string {
-	return []string{}
-}
+// CheckDuplicate searches for duplicate files in the specified directory
+func CheckDuplicate(dir string, delete bool) error{
+	var (
+		compareFiles   = make(map[[sha512.Size]byte]string)
+		duplicateFiles []string
+	)
 
-func FindAllFiles(dir string) {
-	files, err := ioutil.ReadDir(dir)
+	err := filepath.Walk(dir, func(path string, f os.FileInfo, err error) error {
+		if err != nil {
+			panic(err)
+		}
+
+		if f.IsDir() {
+			return nil
+		}
+
+		data, err := ioutil.ReadFile(path)
+		if err != nil {
+			panic(err)
+		}
+
+		hash := sha512.Sum512(data)
+
+		if _, ok := compareFiles[hash]; ok {
+			duplicateFiles = append(duplicateFiles, path)
+		} else {
+			compareFiles[hash] = path
+		}
+		return nil
+	})
+
 	if err != nil {
 		panic(err)
 	}
 
-	for _, file := range files {
-		fmt.Println(file.Sys())
+	if len(duplicateFiles) == 0 {
+		fmt.Println("There are no duplicate files")
+		return nil
+	}
+
+	if len(duplicateFiles) > 0 && delete {
+		deleteDuplicate(duplicateFiles)
+	} else {
+		fmt.Println("List of duplicate files:")
+		for _, val := range duplicateFiles {
+			fmt.Println(val)
+		}
+	}
+	return nil
+}
+
+// deleteDuplicate removes duplicate files found in the specified directory
+func deleteDuplicate(files []string) {
+	var (
+		err error
+		wg  sync.WaitGroup
+	)
+
+	fmt.Println("List of deleted duplicate files:")
+	for _, val := range files {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			err = os.Remove(val)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(val)
+		}()
+		wg.Wait()
 	}
 }
--- main.go
+++ main.go
@@ -1,27 +1,25 @@
+//This program searches for and removes duplicates in the specified directory
+
 package main
 
 import (
-	duplicatefilefinder "DanSu93/duplicate-file-finder/cmd/duplicate-file-finder"
+	duplicateFileFinder "DanSu93/duplicate-file-finder/cmd/duplicate-file-finder"
 	"flag"
 	"os"
 )
 
 var (
 	dir           *string
-	delete        *bool
+	del           *bool
 	currentDir, _ = os.Getwd()
 )
 
 func init() {
 	dir = flag.String("dir", currentDir, "directory in which duplicates are searched")
-	delete = flag.Bool("delete", false, "remove duplicates")
+	del = flag.Bool("del", false, "remove duplicates")
 	flag.Parse()
 }
 
 func main() {
-	if *delete {
-		duplicatefilefinder.DeleteDuplicateFiles(*dir)
-	} else {
-		duplicatefilefinder.FindAllFiles(*dir)
-	}
+	duplicateFileFinder.CheckDuplicate(*dir, *del)
 }
